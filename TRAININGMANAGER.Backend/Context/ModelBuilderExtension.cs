12a31ef0de2f4b77a4667b52a9cc51e1c9e9c448
sz3papdav <sz3papdav@vasvari.org>
2024-04-09 10:25:53 +0200

    Nevek megcsinálva, egy fölös mappa törölve

--- TRAININGMANAGER.Backend/Context/ModelBuilderExtension.cs
+++ TRAININGMANAGER.Backend/Context/ModelBuilderExtension.cs
@@ -53,53 +53,114 @@ namespace TRAININGMANAGER.Backend.Context
                 new Player
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Cigány",
-                    LastName="Zsolt",
-                    BirthsDay=new DateTime(1989,7,10),
-                    Email="farago.zsolt@gmail.com",
+                    FirstName="László",
+                    LastName="Farkas",
+                    BirthsDay=new DateTime(2005,7,30),
+                    Email="laszlo.farkas@gmail.com",
                     AgeGroupId=Guid.NewGuid(),
                     WorkingTypeId=Guid.NewGuid(),
                 },
                 new Player
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Nagy",
-                    LastName="Anita",
-                    BirthsDay=new DateTime(1992,2,23),
-                    Email="nagyanita0223@gmail.com",
+                    FirstName="László",
+                    LastName="Kis",
+                    BirthsDay=new DateTime(2003,4,16),
+                    Email="laszlo.kis@gmail.com",
                     AgeGroupId=Guid.NewGuid(),
                     WorkingTypeId=Guid.NewGuid(),
                 },
                 new Player
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Lakatos",
-                    LastName="Béla",
-                    BirthsDay=new DateTime(2000,11,15),
-                    Email="lakbela@gmail.com",
+                    FirstName="Zsolt",
+                    LastName="Tóth",
+                    BirthsDay=new DateTime(2003,8,20),
+                    Email="zsolt.toth@gmail.com",
                     AgeGroupId=Guid.NewGuid(),
                     WorkingTypeId=Guid.NewGuid(),
                 },
                 new Player
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Kiss",
-                    LastName="László",
-                    BirthsDay=new DateTime(1988,12,1),
-                    Email="farago1998@gmail.com",
+                    FirstName="Dániel",
+                    LastName="Varga",
+                    BirthsDay=new DateTime(2004,7,16),
+                    Email="daniel.varga@gmail.com",
                     AgeGroupId=Guid.NewGuid(),
                     WorkingTypeId=Guid.NewGuid(),
                 },
                 new Player
                 {
                     Id=Guid.NewGuid(),
-                    FirstName="Szekeres",
-                    LastName="Brigitta",
-                    BirthsDay=new DateTime(2002,4,30),
-                    Email="brigitta.szekeres@gmail.com",
+                    FirstName="László",
+                    LastName="Nagy",
+                    BirthsDay=new DateTime(2005,7,5),
+                    Email="laszlo.nagy@gmail.com",
                     AgeGroupId=Guid.NewGuid(),
                     WorkingTypeId=Guid.NewGuid(),
-                }
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Bence",
+                    LastName="Szabó",
+                    BirthsDay=new DateTime(2004,6,10),
+                    Email="bence.szabo@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Áron",
+                    LastName="Papp",
+                    BirthsDay=new DateTime(2003,2,12),
+                    Email="aron.papp@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Zsolt",
+                    LastName="Szabó",
+                    BirthsDay=new DateTime(2003,1,7),
+                    Email="zsolt.szabo@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Dániel",
+                    LastName="Kovács",
+                    BirthsDay=new DateTime(2003,6,25),
+                    Email="daniel.kovacs@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Bence",
+                    LastName="Varga",
+                    BirthsDay=new DateTime(2002,5,6),
+                    Email="bence.varga@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+                new Player
+                {
+                    Id=Guid.NewGuid(),
+                    FirstName="Gergő",
+                    LastName="Kovács",
+                    BirthsDay=new DateTime(2002,11,29),
+                    Email="gergo.kovacs@gmail.com",
+                    AgeGroupId=Guid.NewGuid(),
+                    WorkingTypeId=Guid.NewGuid(),
+                },
+
             };
 
             // Players
